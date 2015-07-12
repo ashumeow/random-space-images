0500af60082508429e1d40f8a44491731d5577b0
ashumeow <aswini1405@outlook.com>
2015-07-12 16:42:38 +0530

    push attempt

--- keyword-search/keyword.py
+++ keyword-search/keyword.py
@@ -5,9 +5,9 @@ def meowFetch():
     # available keywords
     helloMeow = ['hubble','nebula','galaxy']
     # target url
-    meow_url="http://geekresearchlab.net/space/keyword/"
+    meow_url="http://geekresearchlab.net/space/keyword/"+str(helloMeow)+".jpg"
     # URL fetch/retrieve
-    meowCatch=urllib.urlretrieve(meow_url+str(helloMeow)+".jpg")
+    meowCatch=urllib.urlretrieve(meow_url)
     print ("Image fetched")
 
 def meowPath():
@@ -27,6 +27,6 @@ def meow():
     start = raw_input("Available keywords: 'hubble','nebula','galaxy'\n")
     print("Type any one of the available keywords: \n")
     print("Capturing images...\n")
-    meowFetch(int(meowCatch))
+    meowFetch()
     meowPath()
 meow()
