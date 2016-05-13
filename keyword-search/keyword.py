5a49f07ad4cc4d932cf8bef4c7597da24028b6b3
Aswini S <ashumeow@live.com>
2016-05-13 16:01:41 +0400

    Update keyword.py

--- keyword-search/keyword.py
+++ keyword-search/keyword.py
@@ -1,50 +1,25 @@
 import urllib
 import os
-import urllib2
-import json
 
-def meowFetch():
-    # available keywords
-    helloMeow = '''{
-    [
-    'hubble': 'hubble'
-    ],
-    [
-    'nebula': 'nebula'
-    ],
-    [
-    'galaxy': 'galaxy'
-    ]
-    }'''
-    # defining url
-    ig="http://geekresearchlab.net/space/keyword/"
-    # target
-    meow_url=(ig+str(helloMeow)+".jpg".format(helloMeow))
-    # URL fetch/retrieve
-    meowCatch=urllib.urlretrieve(meow_url)
-    # looping
-    for meow_url in meowCatch.read():
-        print("Image fetched")
+def meowFetchImages(start):
+    urllib.urlretrieve("http://geekresearchlab.net/space/hubble/"+helloMeow+".jpg", +helloMeow+".jpg")
+    print("Woo hoo! Finished")
 
 def meowPath():
-    # path locator
     meowCurrent = os.getcwd()
-    # if path doesn't exists
     if not os.path.exists(meowCurrent+"/space/keyword/"):
          print("Wait.. Folder not found!\n")
          print("Creating a new folder...\n")
          os.makedirs(meowCurrent+"/space/keyword/")
          os.chdir(meowCurrent+"/space/keyword/")
     else:
-        # if path exists
          os.chdir(meowCurrent+"/space/keyword/")
 
 def meow():
-    # main execution area
     print("Keyword-based Images Capture\n")
-    helloMeow = raw_input("Available keywords: 'hubble','nebula','galaxy'\n")
-    print("Type any one of the available keywords: \n")
+    start = raw_input("Available keywords: 'hubble','nebula','galaxy'\n")
     print("Capturing images...\n")
     meowPath()
-    meowFetch(int(helloMeow))
+    meowFetchImages(start)
+
 meow()
